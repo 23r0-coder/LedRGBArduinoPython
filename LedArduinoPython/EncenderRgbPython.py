615103977de7aabd1c1074b629cd9f8253b87505
23_ϱ <84030511+23r0-coder@users.noreply.github.com>
2022-03-13 17:16:00 -0600

    Update EncenderRgbPython.py

--- LedArduinoPython/EncenderRgbPython.py
+++ LedArduinoPython/EncenderRgbPython.py
@@ -1,15 +1,18 @@
 from pyfirmata import Arduino, PWM
-import random
-board = Arduino("\\.\COM4")
+import random #libreria para numeros random
+board = Arduino("\\.\COM4") #conexion entre la placa y Python
 
+#pins donde se coloca el led RGB en la placa (Arduino)
 rojo= 9
 verde = 10
 azul = 11
 
+#establece las variables como modulables
 board.digital[rojo].mode = PWM
 board.digital[azul].mode = PWM
 board.digital[verde].mode = PWM
 
+#manda numeros random del 0 al 255 para los colores y da un descanso de 0.5 segs y se ejecuta hasta que se desconecte.
 while True:
    board.digital[rojo].write(random.random())
    board.digital[azul].write(random.random())
