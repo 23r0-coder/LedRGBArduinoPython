7e48bf6106efa085f0dd14adac3f47da5a690ff2
23_ϱ <84030511+23r0-coder@users.noreply.github.com>
2022-03-13 17:17:19 -0600

    Update EncenderLedPython.py

--- LedArduinoPython/EncenderLedPython.py
+++ LedArduinoPython/EncenderLedPython.py
@@ -1,4 +1,4 @@
-import pyfirmata
+import pyfirmata #libreria de conexion
  
 port = "\\.\COM4" #Puerto COM de emulación en USB
 pin = (13) #PIN donde va conectado el LED
@@ -9,11 +9,11 @@ tarjeta = pyfirmata.Arduino(port)
 print("Conectado a Arduino por USB...")
 while True:
     print("Encendiendo LED...")
-    tarjeta.digital[pin].write(1)
+    tarjeta.digital[pin].write(1) #enciende el led
     print("Encendido LED...")
     tarjeta.pass_time(3)
     print("Apagando LED...")
-    tarjeta.digital[pin].write(0)
+    tarjeta.digital[pin].write(0) #apaga el led
     print("Apagado LED...")
     tarjeta.pass_time(3)
 tarjeta.exit()
